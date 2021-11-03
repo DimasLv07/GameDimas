bcd36f0eccf6d4352f9de79511ebc686922c5a2a
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2021-11-03 14:36:30 +0700

    Game

---
+++ script/script2.js
@@ -0,0 +1,78 @@
+var toogled = $("#blok").html('<span oncopy="winner" id="main-block">H<span class="n-1">e</span><span class="n-2">L</span><span class="n-3">L</span><span class="n-4">o</span></span>')
+var win = $("#blok").html('Y<span class="n-1">o</span><span class="n-2">u</span> <span class="n-3">m</span><span class="n-4">a</span><span class="n-5">d</span> <span class="n-2">B</span><span class="n-4">r</span><span class="n-1">o</span><span class="n-2">?</span>')
+var background = $("#blok").css("background-color", "Beige")
+
+
+
+function changeText(text) {
+    var toogledText = text
+    var winText = 'Y<span class="n-1">o</span><span class="n-2">u</span> <span class="n-3">m</span><span class="n-4">a</span><span class="n-5">d</span> <span class="n-2">B</span><span class="n-4">r</span><span class="n-1">o</span><span class="n-2">?</span>'
+    toogled = $("#blok").html(text)
+    if (toogledText === winText) {
+        $("#blok").on("copy", function (){
+            $("body").css("background-color", "green")
+           $("#judul").html('Mantab <span class="n-1">Bang!</span>')
+           $("h2").html("Good")
+           $("h3").html("Bang Jago")
+           $("a").css("display", "block")
+       }) 
+    } else {
+        $("#blok").on("copy", function (){
+            $("body").css("background-color", "red")
+           $("#judul").html('Kampang')
+           $("h2").html("Noob")
+           $("h3").html('<a href="">Salah Gblok</a>')
+           $("a").css("display", "block")
+       })
+    }
+}
+
+$(".btn-1").on("click mouseover", () =>  {
+    changeText('G<span class="n-1">o</span><span class="n-2">o</span><span class="n-3">d</span>')
+});
+
+$(".btn-2").on("click mouseover",  () => {
+    changeText('Hi<span class="n-1">B</span><span class="n-2">ro</span>')
+});
+
+$(".btn-3").on("click mouseover", () => {
+    changeText('S<span class="n-1">e</span><span class="n-2">arch it</span>')
+})
+
+$(".btn-4").on("click mouseover",  () => {
+    changeText('Y<span class="n-1">o</span><span class="n-2">u</span> <span class="n-3">m</span><span class="n-4">a</span><span class="n-5">d</span> <span class="n-2">B</span><span class="n-4">r</span><span class="n-1">o</span><span class="n-2">?</span>')
+})
+
+$(".btn-5").on("click mouseover",  () => {
+    changeText('H<span class="n-1">i</span><span class="n-2">l</span><span class="n-3">a</span><span class="n-4">n</span><span class="n-1">g</span> </span><span class="n-3">o</span></span><span class="n-1">t</span></span><span class="n-4">a</span></span><span class="n-1">k</span></span><span class="n-3">n</span></span><span class="n-2">y</span></span><span class="n-1">a</span>')
+})
+
+
+
+
+
+
+
+
+$(document).on("keypress", function (event) {
+    switch (event.key) {
+        case "a":
+            changeText('G<span class="n-1">o</span><span class="n-2">o</span><span class="n-3">d</span>')
+            break;
+        case "s":
+            changeText('Hi<span class="n-1">B</span><span class="n-2">ro</span>')
+            break;
+        case "d":
+            changeText('S<span class="n-1">e</span><span class="n-2">arch it</span>')
+            break;
+        case "f":
+            changeText('Y<span class="n-1">o</span><span class="n-2">u</span> <span class="n-3">m</span><span class="n-4">a</span><span class="n-5">d</span> <span class="n-2">B</span><span class="n-4">r</span><span class="n-1">o</span><span class="n-2">?</span>')
+            break;
+        case "g":
+            changeText('H<span class="n-1">i</span><span class="n-2">l</span><span class="n-3">a</span><span class="n-4">n</span><span class="n-1">g</span> </span><span class="n-3">o</span></span><span class="n-1">t</span></span><span class="n-4">a</span></span><span class="n-1">k</span></span><span class="n-3">n</span></span><span class="n-2">y</span></span><span class="n-1">a</span>')
+            break;
+        default:
+            break;
+    }
+})
+
